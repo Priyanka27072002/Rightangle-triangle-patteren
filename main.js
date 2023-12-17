0ad4c6c842d7113db13ff879d5f7363da4357d6a
fgjer <priyankan27072002@gmail.com>
2023-12-16 17:05:32 -0800

    addnew

---
+++ main.js
@@ -0,0 +1,11 @@
+var n=parseInt(prompt("Enter the number"))
+var s=0
+for(i=1;i<=n;i++){
+    
+    for(j=1;j<=i;j++){
+      s=s+1  
+        document.write(s+" ")
+    }
+   
+    document.write("<br>")
+}
